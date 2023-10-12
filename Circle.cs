98b6ef7ee5bce0df5eb278575fb726d782a76a45
Patrik <144246738+PatpetEdu@users.noreply.github.com>
2023-10-12 18:11:54 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_5__Grunderna_OOP
+{
+    internal class Circle
+    {
+        private float _pi = 3.141f;
+        private int _Radius;
+
+        public Circle(int radius)
+        {         
+            _Radius = radius;
+        }
+
+        public float getArea()
+        {
+            
+            Console.WriteLine($"Arean för circeln med radie {_Radius} är: {_pi * _Radius * _Radius} ");
+            return _pi * _Radius * _Radius;
+            
+           
+        }
+    }
+    
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb_5__Grunderna_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle Circle1 = new Circle(5);
+            Circle1.getArea();
+            
+
+            Circle Circle2 = new Circle(6);
+            Circle2.getArea();
+
+        }
+    }
+}
