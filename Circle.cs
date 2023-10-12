de92414005310c1b8f640bd2c7736b89aadab55d
Patrik <144246738+PatpetEdu@users.noreply.github.com>
2023-10-12 18:46:55 +0200

    lagt till kommentarer

--- Circle.cs
+++ Circle.cs
@@ -8,21 +8,18 @@ namespace Labb_5__Grunderna_OOP
 {
     internal class Circle
     {
+        //varibels to store the values
         private float _pi = 3.141f;
-        private int _Radius;
-
+        private int _Radius;    
         public Circle(int radius)
         {         
             _Radius = radius;
         }
-
-        public float getArea()
-        {
-            
+        //Method getArea calculates the area in a circle and prints the result
+        public float getArea() 
+        {        
             Console.WriteLine($"Arean för circeln med radie {_Radius} är: {_pi * _Radius * _Radius} ");
-            return _pi * _Radius * _Radius;
-            
-           
+            return _pi * _Radius * _Radius; //returns value                   
         }
     }
     
--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Circle Circle1 = new Circle(5);
-            Circle1.getArea();
-            
+            Circle Circle1 = new Circle(5);//sets the value of radius from the constructor
+            Circle1.getArea();//calls the method getArea
 
-            Circle Circle2 = new Circle(6);
-            Circle2.getArea();
+
+            Circle Circle2 = new Circle(6);//sets the value of radius from the constructor
+            Circle2.getArea();//calls the method getArea
 
         }
     }
